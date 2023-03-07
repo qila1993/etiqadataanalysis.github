e14cee26c839581900464376a5c17d4dc361d303
qila1993 <118425967+qila1993@users.noreply.github.com>
2023-03-07 15:24:02 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,27 +1,20 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-
-st.header("My Streamlit App")
-
-import streamlit as st
 import pandas as pd
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
-# Simple Advertising Prediction App
-
+# Simple Advertising  Prediction App
 This app predicts the **Advertising** type!
 """)
 
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    TV_ad = st.sidebar.slider('TV ad', 0.7, 296.4, 177)
+    TV_ad = st.sidebar.slider('TV ad', 0.7, 296.4, 177.0)
     Radio_ad = st.sidebar.slider('Radio ad', 0, 49.6, 28.7)
-    Newspaper_ad = st.sidebar.slider('Newspaper ad', 0.3, 114, 8.1)
-    Sales_ad = st.sidebar.slider('Sales ad', 1.6, 27, 19.6)
+    petal_length = st.sidebar.slider('Newspaper ad', 0.3, 114.0, 8.1)
+    petal_width = st.sidebar.slider('Sales ad', 1.6, 27.0, 19.6)
     data = {'TV_ad': TV_ad,
             'Radio_ad': Radio_ad,
             'Newspaper_ad': Newspaper_ad,
@@ -34,7 +27,7 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-advertising = datasets.load_advertising()
+iris = datasets.load_advertising()
 X = advertising.data
 Y = advertising.target
 
@@ -45,10 +38,10 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(advertising.target_names)
+st.write(iris.target_names)
 
 st.subheader('Prediction')
-st.write(advertising.target_names[prediction])
+st.write(iris.target_names[prediction])
 #st.write(prediction)
 
 st.subheader('Prediction Probability')
