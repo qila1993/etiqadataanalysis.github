00accca47ab357f6491938eb27600fa746a973d1
qila1993 <118425967+qila1993@users.noreply.github.com>
2023-03-07 16:05:36 +0800

    Update myapp (1).py

--- myapp (1).py
+++ myapp (1).py
@@ -1,9 +1,48 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
 
-st.header("My Streamlit App")
-st.write(pd.DataFrame({
-    'Intplan': ['yes', 'yes', 'yes', 'no'],
-    'Churn Status': [0, 0, 0, 1]
-}))
+st.write("""
+# Simple Advertising  Prediction App
+This app predicts the **Advertising** type!
+""")
+
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    TV_ad = st.sidebar.slider('TV', 0.7, 296.4, 177.0)
+    Radio_ad = st.sidebar.slider('Radio', 0.0, 49.6, 28.7)
+    Newspaper_ad = st.sidebar.slider('Newspaper', 0.3, 114.0, 8.1)
+    Sales_ad = st.sidebar.slider('Sales', 1.6, 27.0, 19.6)
+    data = {'TV_ad': TV_ad,
+            'Radio_ad': Radio_ad,
+            'Newspaper_ad': Newspaper_ad,
+            'Sales_ad': Sales_ad}
+    features = pd.DataFrame(data, index=[10])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+advertising = pd.read_csv('Advetising.csv')
+X = advertising.data
+Y = advertising.target
+
+clf = RandomForestClassifier()
+clf.fit(X, Y)
+
+prediction = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
+
+st.subheader('Class labels and their corresponding index number')
+st.write(advertising.target_names)
+
+st.subheader('Prediction')
+st.write(advertising.target_names[prediction])
+#st.write(prediction)
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
