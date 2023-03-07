079e0e01808956deaba82d44abb4881f97d19cf8
qila1993 <118425967+qila1993@users.noreply.github.com>
2023-03-07 15:31:15 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -11,10 +11,10 @@ This app predicts the **Advertising** type!
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    TV_ad = st.sidebar.slider('TV ad', 0.7, 296.4, 177.0)
-    Radio_ad = st.sidebar.slider('Radio ad', 0, 49.6, 28.7)
-    Newspaper_ad = st.sidebar.slider('Newspaper ad', 0.3, 114.0, 8.1)
-    petal_width = st.sidebar.slider('Sales ad', 1.6, 27.0, 19.6)
+    TV_ad = st.sidebar.slider('TV', 0.7, 296.4, 177.0)
+    Radio_ad = st.sidebar.slider('Radio', 0, 49.6, 28.7)
+    Newspaper_ad = st.sidebar.slider('Newspaper', 0.3, 114.0, 8.1)
+    petal_width = st.sidebar.slider('Sales', 1.6, 27.0, 19.6)
     data = {'TV_ad': TV_ad,
             'Radio_ad': Radio_ad,
             'Newspaper_ad': Newspaper_ad,
