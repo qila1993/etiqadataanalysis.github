f56e522aa64d0c8a5a7c493404929d9cd3f60392
qila1993 <118425967+qila1993@users.noreply.github.com>
2023-03-07 15:29:12 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -13,7 +13,7 @@ st.sidebar.header('User Input Parameters')
 def user_input_features():
     TV_ad = st.sidebar.slider('TV ad', 0.7, 296.4, 177.0)
     Radio_ad = st.sidebar.slider('Radio ad', 0, 49.6, 28.7)
-    petal_length = st.sidebar.slider('Newspaper ad', 0.3, 114.0, 8.1)
+    Newspaper_ad = st.sidebar.slider('Newspaper ad', 0.3, 114.0, 8.1)
     petal_width = st.sidebar.slider('Sales ad', 1.6, 27.0, 19.6)
     data = {'TV_ad': TV_ad,
             'Radio_ad': Radio_ad,
