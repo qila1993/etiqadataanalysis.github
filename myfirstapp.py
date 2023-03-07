4b2549cf5b6ae0bb0bc231dae412f1ce8aa7f82c
qila1993 <118425967+qila1993@users.noreply.github.com>
2023-03-07 12:15:56 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -8,7 +8,7 @@ show = st.checkbox('I agree the terms and conditions')
 
 option = st.sidebar.selectbox(
     'Select a mini project',
-     ['line chart','map','T n C'])
+     ['line chart','map','T n C','Long Process'])
 
 if option=='line chart':
     chart_data = pd.DataFrame(
@@ -23,8 +23,8 @@ elif option=='map':
     columns=['lat', 'lon'])
 
     st.map(map_data)
-    
-else:
+
+elif option=='T n C':
     st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
     show = st.checkbox('I agree the terms and conditions')
     if show:
@@ -33,3 +33,16 @@ else:
         'Churn Status': [0, 0, 0, 1]
         }))
 
+else:
+    'Starting a long computation...'
+    
+    latest_iteration = st.empty()
+    bar = st.progress(0)
+
+    for i in range(100):
+   
+        latest_iteration.text(f'Iteration {i+1}')
+        bar.progress(i + 1)
+        time.sleep(0.1)
+
+    '...and now we\'re done!'
